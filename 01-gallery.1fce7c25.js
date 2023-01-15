fcaa9ad89b96f2a8a983d300cd252f0a20675749
Stanislav-She <116562198+Stanislav-She@users.noreply.github.com>
2023-01-15 22:56:13 +0000

    Deploying to gh-pages from @ Stanislav-She/goit-js-hw-08@92a5b88320dc14d60be02f815de5cbdf6d84f6f1 🚀

---
+++ 01-gallery.1fce7c25.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".gallery");console.log(e)}();
+//# sourceMappingURL=01-gallery.1fce7c25.js.map
---
+++ 01-gallery.399f9140.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".gallery");console.log(e);
+//# sourceMappingURL=01-gallery.399f9140.js.map
