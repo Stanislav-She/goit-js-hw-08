4bb3c3928082b881e980034bf4dc63acf9dfaa9d
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-17 19:24:47 +0200

    Adds many components:
    - npm library @vimeo/player;
    - npm library lodash.throttle;
    - npm library simplelightbox;
    - finalized task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,71 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const containerGalleryRef = document.querySelector('.gallery');
-console.log(containerGalleryRef);
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+containerGalleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+// import SimpleLightbox from 'simplelightbox';
+// // додаємо імпорт бібліотеки SimpleLightbox
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// // додаємо ще один додатковий імпорт стилів бібліотеки SimpleLightbox
+
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+
+// // решта коду внизу зкопійована з task-02 home work 7
+
+// const containerGalleryRef = document.querySelector('.gallery');
+
+// const galleryMarkup = createGalleryMarkup(galleryItems);
+
+// containerGalleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// function createGalleryMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//         <a class="gallery__item" href="${original}">
+//             <img
+//             class="gallery__image"
+//             src="${preview}"
+//             alt="${description}"
+//             />
+//         </a>`;
+//     })
+//     .join('');
+// }
+
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionPosition: 'bottom',
+//   captionDelay: 250,
+// });
